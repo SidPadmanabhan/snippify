241bedeca7286391c57ecbda0c289bb66c2c6991
SidPadmanabhan <88408627+SidPadmanabhan@users.noreply.github.com>
2025-12-10 12:28:48 -0500

    finished creating snippet entity class in snippet.java

--- backend/src/main/java/com/snippify/snippify/controller/SnippetController.java
+++ backend/src/main/java/com/snippify/snippify/controller/SnippetController.java
@@ -1,5 +1,17 @@
 package com.snippify.snippify.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/snippets")
 public class SnippetController {
-    
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello from Snippify!";
+    }
+
 }
+
